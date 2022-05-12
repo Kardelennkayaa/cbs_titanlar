216b6dd95ff6f02d59ea52eeb2996d8b463cb9da
Kardelennkayaa <76513887+Kardelennkayaa@users.noreply.github.com>
2022-05-12 14:39:58 +0300

    Delete data.php

--- data.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'db.php';
-
-$get_sql = "SELECT location, recorder_name, ST_AsText(geom) as geom_text, minibus_name, distance, driver, contact, start_time, path_def FROM public.hacettepe_servis";
-$resultArray = pg_fetch_all(pg_query($dbcon,$get_sql));
-echo json_encode($resultArray);
-?>
